8bf8ab18ec98635caf3a6e8db38048a511b8b2a1
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-07 19:04:21 +0530

    spacing and content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
-<h1 class="text-3xl font-bold underline">
-  Technetium Hackathon
-</h1>
+<div class="container mx-auto"  >
+    <h1 class="text-3xl font-bold underline">
+        Technetium Hackathon
+      </h1>
+<p> 
+    Technetium is a 3-day hackathon that brings together students from all over the city to collaborate on innovative projects and ideas. The event is focused on fostering a collaborative and inclusive environment where participants can learn from one another, share their skills, and build something meaningful.
+
+    <br><br>Throughout the hackathon, attendees will have access to a variety of resources, including mentorship from industry experts, workshops and talks from leaders in the tech industry, and a supportive community of like-minded individuals. In addition, there will be a number of challenges and prizes available to participants, providing an opportunity to showcase their skills and potentially win recognition and rewards.
+    
+    <br><br>Whether you're a seasoned hacker or a beginner looking to learn more about the world of technology, Technetium is the perfect event to connect with others, explore new ideas, and make a real impact. So, if you want to be a part of this amazing event, don't wait and register now! 
+</p>
+</div>
