45191f8e1d4ea7dc1b860558bf5a07cf001ba691
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-07 23:04:27 +0530

    header component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,29 +1,16 @@
 <script>
  // @ts-ignore
  import pico from "@picocss/pico"
- let typeEffect = 'typing-dmeo'
+ import Header from "$lib/header.svelte"; 
 </script>
-<nav class="container mx-auto">
-    <div class="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
-        <ul class="navbar-nav mr-auto lg:flex lg:flex-row">
-          <li class="nav-item">
-            <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Dashboard</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Team</a>
-          </li>
-          <li class="nav-item mb-2 lg:mb-0">
-            <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Projects</a>
-          </li>
-        </ul>
-      </div>
-</nav>
 
+
+<Header />
 
 <slot/>
 
 <div class=" container mx-auto text-right">
-     <footer class=""> Technetium 2023</footer>
+     <footer> Technetium 2023</footer>
 </div>
 
 
