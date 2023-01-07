09d9250ae38f4f0b71b6c83c83afa58324b491d4
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-07 18:35:31 +0530

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,22 @@
 <script>
-    import "../app.css";
-  </script>
-  <slot />
+    import pico from "@picocss/pico"
+</script>
+<nav>
+    <a prefetch href="/"> My site</a>
+</nav>
+
+
+<slot/>
+
+
+<footer> Technetium 2023</footer>
+
+<style>
+    nav{
+        background : (0, 0, 0, 0.4);
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        letter-spacing: 0.1px;
+        font-weight: 500;
+}
+</style>
