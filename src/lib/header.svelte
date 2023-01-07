51eb1e1c73fe44feb4d430887a7b86e24f761a0b
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-07 23:05:27 +0530

    header component

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -16,3 +16,14 @@
       </div>
 </nav>
 </header>
+
+<style>
+    nav{
+        background : (0, 0, 0, 0.4);
+        padding-top: 1rem;
+        padding-bottom: 1rem;
+        
+        letter-spacing: 0.1px;
+        font-weight: 500;
+}
+</style>
