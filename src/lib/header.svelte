a56d8ac960ec853f6a5979c03491e646c835d831
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-08 10:18:28 +0530

    tailwind

--- src/lib/header.svelte
+++ src/lib/header.svelte
@@ -7,11 +7,7 @@
             <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="/" data-mdb-ripple="true" data-mdb-ripple-color="light">Dashboard</a>
           </li>
           <li class="nav-item">
-<<<<<<< HEAD
             <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="/routes/team" data-mdb-ripple="true" data-mdb-ripple-color="light">Team</a>
-=======
-            <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="/team" data-mdb-ripple="true" data-mdb-ripple-color="light">Team</a>
->>>>>>> d58cb6f17bd849ef78e2efeba929e2bd3d269554
           </li>
           <li class="nav-item mb-2 lg:mb-0">
             <a class="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Projects</a>
