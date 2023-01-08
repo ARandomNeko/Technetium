43aa2ff2ed47843584b0bfa0894e4b23c773ef19
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-08 21:33:23 +0530

    renamed the team folder in source

--- .svelte-kit/generated/client-manifest.js
+++ .svelte-kit/generated/client-manifest.js
@@ -9,7 +9,7 @@ export const server_loads = [];
 
 export const dictionary = {
 	"/": [2],
-	"/Team": [3]
+	"/team": [3]
 };
 
 export const hooks = {
--- .svelte-kit/generated/nodes/3.js
+++ .svelte-kit/generated/nodes/3.js
@@ -1,1 +1,1 @@
-export { default as component } from "../../../src/routes/Team/+page.svelte";
+export { default as component } from "../../../src/routes/team/+page.svelte";
