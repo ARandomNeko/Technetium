ef4012335baf996de5e83357c8bc00fa829b7d69
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-08 10:53:46 +0530

    animations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,11 @@
 <script>
- import "../app.css";
+import Header from "$lib/Header.svelte"; 
  import "@picocss/pico/css/pico.css";
- import Header from "$lib/header.svelte"; 
+import "../app.css";
 </script>
 
 
- <header>
-     <nav class="container mx-auto">
-          <div class="navbar-collapse collapse grow items-center" id="navbarSupportedContentY">
-              <ul class="navbar-nav mr-auto lg:flex lg:flex-row">
-                <li class="nav-item">
-                  <a class="nav-link block pr-2 lg:px-2 py-2 text-white hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="/" data-mdb-ripple="true" data-mdb-ripple-color="light">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link block pr-2 lg:px-2 py-2 text-white hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="/routes/team" data-mdb-ripple="true" data-mdb-ripple-color="light">Team</a>
-                </li>
-                <li class="nav-item mb-2 lg:mb-0">
-                  <a class="nav-link block pr-2 lg:px-2 py-2 text-white hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">Projects</a>
-                </li>
-              </ul>
-            </div>
-      </nav>
- </header>
+<Header />
 
 <slot/>
 
@@ -31,5 +15,6 @@
 
 
 <style>
-   
+    
+ 
  </style>
