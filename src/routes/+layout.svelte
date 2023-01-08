e947d26c946c156fa3571febd836c7dbd2cc8423
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-08 12:44:03 +0530

    animations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,7 +20,7 @@ import "../app.css";
        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
          <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
            <li>
-             <a href="#!" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
+             <a href="/" class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
            </li>
            <li>
              <a href="/Team" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Team</a>
@@ -43,12 +43,25 @@ import "../app.css";
 
 <slot/>
 
-<div class =" container mx-auto text-right">
-     <footer class ="text-right"> Technetium 2023.</footer>
+<div class =" container mx-auto text-right translate-x-40">
+     <footer class =" text-right"> 
+          <a href="/">Technetium 2023.</a>
+     </footer>
 </div>
 
 
 <style>
-    
+    a {
+  box-shadow: inset 0 0 0 0 #54b3d6;
+  color: #54b3d6;
+  padding: 0 .25rem;
+  margin: 0 -.25rem;
+  transition: color .3s ease-in-out, box-shadow .3s ease-in-out;
+}
+a:hover {
+  color: #fff;
+  box-shadow: inset 200px 0 0 0 #54b3d6;;
+}
+
  
  </style>
