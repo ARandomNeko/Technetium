3dc1852de4d5aab66f429f163a10523e2b809ce6
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-01-09 12:03:25 +0530

    removed the password input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,7 +27,6 @@
                     </div>
                   </div>
                   <input type="email" class="form-control block w-full px-3 py-1.5 mb-6 text-base font-normal text-gray-700 bg-slate-100 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-slate-200 focus:border-blue-600 focus:outline-none" placeholder="Email address"/>
-                  <input type="password" class="form-control block w-full px-3 py-1.5 mb-6 text-base font-normal text-gray-700 bg-slate-100 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-slate-200 focus:border-blue-600 focus:outline-none" placeholder="Password"/>
                   <div class="form-check flex justify-center mb-6">
                     
                   </div>
