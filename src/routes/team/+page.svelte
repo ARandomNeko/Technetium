860af0331d21a2bc14167537239308de28f4a47c
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-09 17:19:51 +0530

    created a sample team card

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,5 +1,9 @@
 <div class="team-card">
     <div class="team-card_load"></div>
-    <div class="team-card_load_extreme_title"></div>
-    <div class="team-card_load_extreme_descripion"></div>
+    <div class="team-card_load_extreme_title">
+        <p class=" text-gray-700 text-left text-sm translate-x-2">Rituparan Reddy</p>
+    </div>
+    <div class="team-card_load_extreme_descripion">
+        <p class=" text-gray-700 text-left text-sm translate-x-2">Lead dev<br> Problem Setter<br>Judge</p>
+    </div>
 </div>
