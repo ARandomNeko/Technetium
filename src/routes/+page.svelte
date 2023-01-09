7ce08acd836c94bc938bc9bb806adc50c52361b4
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-09 16:59:32 +0530

    changed the home page a bit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,24 @@
-<div class="mb-20"  style = "background-image: url('https://wallpapersmug.com/download/1600x900/dbc266/texture-dark-surface.jpg');background-repeat: no-repeat;  background-size: cover;">
+<div class="mb-20 bg-black"  >
 
     <!-- Section: Design Block -->
     <section class="mb-20 translate-y-12">
      
   
-      <div class="px-6 py-12 md:px-12 bg-black text-gray-800 text-left lg:text-left block rounded-lg shadow-lg">
+      <div style = "background-image: url('https://wallpapersmug.com/download/1600x900/dbc266/texture-dark-surface.jpg');background-repeat: no-repeat;  background-size: cover;"class="px-6 py-12 md:px-12 bg-black text-gray-800 text-left lg:text-left block rounded-lg shadow-lg">
         <div class="container mx-auto xl:px-32">
           <div class="grid lg:grid-cols-2 gap-12 items-center">
-            <div class="mt-12 lg:mt-0">
+            <div class="mt-12 lg:mt-0 py-4">
               <h1 class="typing-title text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-1">Sreenidhi Technetium </h1>
-              <h1 class=" text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight text-blue-600 mb-1">The first student led hackathon</h1>
+              <h1 class=" text-xl md:text-5xl xl:text-6xl  tracking-tight font-semibold text-blue-500 mb-1">The first student led hackathon in Hyderabad</h1>
               <p class="text-gray-450">
                 Technetium is a 3-day hackathon that brings together students from all over the city to collaborate on innovative projects and ideas. 
                 The event is focused on fostering a collaborative and inclusive environment where participants can learn from one another, share their skills, and build something meaningful. 
               </p>
             </div>
-            <div class="mb-8 lg:mb-0">
-              <div class="block rounded-lg shadow-lg bg-gray-800 px-2 py-12 md:px-12 translate-y-12">
+            <div class="mb-6 lg:mb-0 translate-y-6">
+              <div class="block rounded-lg shadow-lg bg-gray-800 px-2 py-8 md:px-12 translate-y-4">
                 <form >
-                  <div class="grid md:grid-cols-2 md:gap-6">
+                  <div class="grid md:grid-cols-2 md:gap-6 translate-y-3">
                     <div class="mb-1">
                       <input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-slate-100 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-slate-200 focus:border-blue-600 focus:outline-none" placeholder="First name"/>
                     </div>
@@ -26,14 +26,14 @@
                       <input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-slate-100 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-slate-200 focus:border-blue-600 focus:outline-none" placeholder="Last name"/>
                     </div>
                   </div>
-                  <input type="email" class="form-control block w-full px-3 py-1.5 mb-6 text-base font-normal text-gray-700 bg-slate-100 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-slate-200 focus:border-blue-600 focus:outline-none" placeholder="Email address"/>
-                  <div class="form-check flex justify-center mb-6">
+                  <input type="email" class="translate-y-3 form-control block w-full px-3 py-1.5 mb-6 text-base font-normal text-gray-700 bg-slate-100 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-slate-200 focus:border-blue-600 focus:outline-none" placeholder="Email address"/>
+                  <div class="form-check flex justify-center mb-1">
                     
                   </div>
                   <button 
-                  type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block px-6 py-2.5 mb-6 w-full bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                  type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="translate-y-3 inline-block px-6 py-4  w-full bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                   Register
-                </button>
+                  </button>
                  
                 </form>
               </div>
