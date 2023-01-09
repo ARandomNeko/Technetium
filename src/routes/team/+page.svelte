0415dce424b23f97599cd4fc5272d73eebcb2e25
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-09 19:10:49 +0530

    spacing

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,4 +1,6 @@
-<div class="team-card">
+<div class="bg-black mb-0 ">
+<section class="bg-black py-40 mx-auto">
+<div class="team-card py-12 " style="transform: translate(-750px,-275px);" >
     <div class="team-card_load"></div>
     <div class="team-card_load_extreme_title">
         <p class=" text-gray-700 text-left text-sm translate-x-2">Rituparan Reddy</p>
@@ -6,4 +8,6 @@
     <div class="team-card_load_extreme_descripion">
         <p class=" text-gray-700 text-left text-sm translate-x-2">Lead dev<br> Problem Setter<br>Judge</p>
     </div>
+</div>
+</section>
 </div>
