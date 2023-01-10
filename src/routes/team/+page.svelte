7b7272914dead9cdcf24d8655bfd63d979869b70
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-01-10 08:14:42 +0530

    card

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,8 +1,6 @@
-
-
-<figure class="md:flex  rounded-xl p-8 md:p-0 bg-slate-800">
+<figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
   <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512">
-  <div class="pt-6 md:p-8 text-left md:text-left space-y-4">
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p class="text-lg font-medium">
         “Tailwind CSS is the only framework that I've seen scale
@@ -11,10 +9,10 @@
       </p>
     </blockquote>
     <figcaption class="font-medium">
-      <div class="text-sky-400">
+      <div class="text-sky-500 dark:text-sky-400">
         Sarah Dayan
       </div>
-      <div class="text-slate-500">
+      <div class="text-slate-700 dark:text-slate-500">
         Staff Engineer, Algolia
       </div>
     </figcaption>
