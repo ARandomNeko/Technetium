f80cda9a591656ac23d7ca1cf727d3b22be63b00
ARandomNeko <rituparangreddy2006@gmail.com>
2023-01-10 16:49:30 +0530

    new better team cards

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,48 +1,207 @@
+<!-- MDI Icons -->
+<link rel="stylesheet" href="https://cdn.materialdesignicons.com/6.5.95/css/materialdesignicons.min.css">
 
-<div class="-mb-2 bg-black"  >
+<!-- Page Container -->
+<div class="flex items-center justify-center min-h-screen bg-black py-48">
+    <div class="flex flex-col">
+        <!-- Notes -->
+       
 
-<section class="mb-20 translate-y-10">
-     
-  
-<div style = "background-image: url('https://wallpapersmug.com/download/1600x900/dbc266/texture-dark-surface.jpg');background-repeat: no-repeat;  background-size: cover;"class="px-6 py-12 md:px-12 bg-black text-gray-800 text-left lg:text-left block rounded-lg shadow-lg">
-<div class="container mx-auto xl:px-32">
-<div class="grid lg:grid-cols-2 gap-12 items-center">
-<div class="mt-12 lg:mt-0 py-4">
-<div class="bg-black mb-0 py-40">
+        <div class="flex flex-col mt-8">
+            <!-- Meet the Team -->
+            <div class="container max-w-7xl px-4">
+                <!-- Section Header -->
+                <div class="flex flex-wrap justify-center text-center mb-24">
+                    <div class="w-full lg:w-6/12 px-4">
+                        <!-- Header -->
+                        <h1 class="text-gray-100 text-4xl font-bold mb-8">
+                            Meet the Team
+                        </h1>
 
-<<<<<<< HEAD
-<section class="container py-40 px-24 ">
-<div class=" px-24 team-card-ritu py-12 border-2 border-pink-400   " style="transform: translate(-750px,-275px);" >
-=======
-<div class="team-card-ritu py-12 border-2 border-pink-400   " style="transform: translate(-750px,-275px);" >
->>>>>>> 28948929e56145eff554a5f10dbee7be4dcbed33
-    <div class="team-card_load"></div>
-    <div class="team-card_load_extreme_title">
-        <p class=" text-gray-700 text-left text-sm translate-x-2">Rituparan Reddy</p>
+                        <!-- Description -->
+                        <p class="text-gray-00 text-lg font-light">
+                            With over 100 years of combined experience, we've got a well-seasoned team at the helm.
+                        </p>
+                    </div>
+                </div>
+
+                <!-- Team Members -->
+                <div class="flex flex-wrap">
+                    <!-- Member   #!1 -->
+                    <div class=" py-4 w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4 hover:scale-105  hover:drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] bg-gray-800  hover:rounded-xl transition ease-in-out duration-700">
+                        <div class="flex flex-col">
+                            <!-- Avatar -->
+                            <a href="  #!" class="mx-auto">
+                                <img class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                    src="https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?fit=clamp&w=400&h=400&q=80">
+                            </a>
+
+                            <!-- Details -->
+                            <div class="text-center mt-6">
+                                <!-- Name -->
+                                <h1 class="text-slate-300 text-xl font-bold mb-1">
+                                    Rituparan Reddy
+                                </h1>
+
+                                <!-- Title -->
+                                <div class="text-slate-400 font-light mb-2">
+                                    Lead developer & Judge
+                                </div>
+
+                                <!-- Social Icons -->
+                                <div class="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+                                    <!-- Linkedin -->
+                                    <a href=" https://github.com/ARandomNeko" class="flex rounded-2xl hover:bg-indigo-50 h-10 w-10">
+                                        <i class="mdi mdi-github text-indigo-500 mx-auto mt-2"></i>
+                                    </a>
+
+                                    <!-- Twitter -->
+                                    <a href="https://twitter.com/ARandomNeko21" class="flex rounded-2xl hover:bg-blue-50 h-10 w-10">
+                                        <i class="mdi mdi-twitter text-blue-300 mx-auto mt-2"></i>
+                                    </a>
+
+                                    <!-- Instagram -->
+                                    <a href=" https://www.instagram.com/arandomneko21/" class="flex rounded-2xl hover:bg-orange-50 h-10 w-10">
+                                        <i class="mdi mdi-instagram text-orange-400 mx-auto mt-2"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Member   #!2 -->
+                    <div class=" py-4 w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4 hover:scale-105 transition ease-in-out duration-700 hover:drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)] bg-gray-800  hover:rounded-xl">
+                        <div class="flex flex-col">
+                            <!-- Avatar -->
+                            <a href="  #!" class="mx-auto">
+                                <img class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                    src="https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?fit=clamp&w=400&h=400&q=80">
+                            </a>
+
+                            <!-- Details -->
+                            <div class="text-center mt-6">
+                                <!-- Name -->
+                                <h1 class="text-slate-300 text-xl font-bold mb-1">
+                                    Sahith Gandham
+                                </h1>
+
+                                <!-- Title -->
+                                <div class="text-slate-400 font-light mb-2">
+                                    Head of logistics & Asistant Developer
+                                </div>
+
+                                <!-- Social Icons -->
+                                <div class="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+                                    <!-- Linkedin -->
+                                    <a href=" https://github.com/ARandomNeko" class="flex rounded-2xl hover:bg-indigo-50 h-10 w-10">
+                                        <i class="mdi mdi-github text-indigo-500 mx-auto mt-2"></i>
+                                    </a>
+
+                                    <!-- Twitter -->
+                                    <a href="https://twitter.com/ARandomNeko21" class="flex rounded-2xl hover:bg-blue-50 h-10 w-10">
+                                        <i class="mdi mdi-twitter text-blue-300 mx-auto mt-2"></i>
+                                    </a>
+
+                                    <!-- Instagram -->
+                                    <a href=" https://www.instagram.com/arandomneko21/" class="flex rounded-2xl hover:bg-orange-50 h-10 w-10">
+                                        <i class="mdi mdi-instagram text-orange-400 mx-auto mt-2"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <!-- Member   #!3 -->
+                    <div class="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                        <div class="flex flex-col">
+                            <!-- Avatar -->
+                            <a href="  #!" class="mx-auto">
+                                <img class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                    src="https://images.unsplash.com/photo-1634193295627-1cdddf751ebf?fit=clamp&w=400&h=400&q=80">
+                            </a>
+
+                            <!-- Details -->
+                            <div class="text-center mt-6">
+                                <!-- Name -->
+                                <h1 class="text-gray-900 text-xl font-bold mb-1">
+                                    Kenji Milton
+                                </h1>
+
+                                <!-- Title -->
+                                <div class="text-gray-700 font-light mb-2">
+                                    Team Memeber
+                                </div>
+
+                                <!-- Social Icons -->
+                                <div class="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+                                    <!-- Linkedin -->
+                                    <a href="  #!" class="flex rounded-full hover:bg-indigo-50 h-10 w-10">
+                                        <i class="mdi mdi-linkedin text-indigo-700 mx-auto mt-2"></i>
+                                    </a>
+
+                                    <!-- Twitter -->
+                                    <a href="  #!" class="flex rounded-full hover:bg-blue-50 h-10 w-10">
+                                        <i class="mdi mdi-twitter text-blue-400 mx-auto mt-2"></i>
+                                    </a>
+
+                                    <!-- Instagram -->
+                                    <a href="  #!" class="flex rounded-full hover:bg-orange-50 h-10 w-10">
+                                        <i class="mdi mdi-instagram text-orange-400 mx-auto mt-2"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Member   #!4 -->
+                    <div class="w-full md:w-6/12 lg:w-3/12 mb-6 px-6 sm:px-6 lg:px-4">
+                        <div class="flex flex-col">
+                            <!-- Avatar -->
+                            <a href="  #!" class="mx-auto">
+                                <img class="rounded-2xl drop-shadow-md hover:drop-shadow-xl transition-all duration-200 delay-100"
+                                    src="https://images.unsplash.com/photo-1635003913011-95971abba560?fit=clamp&w=400&h=400&q=80">
+                            </a>
+
+                            <!-- Details -->
+                            <div class="text-center mt-6">
+                                <!-- Name -->
+                                <h1 class="text-gray-900 text-xl font-bold mb-1">
+                                    Doesn't matter
+                                </h1>
+
+                                <!-- Title -->
+                                <div class="text-gray-700 font-light mb-2">
+                                    Will be fired
+                                </div>
+
+                                <!-- Social Icons -->
+                                <div class="flex items-center justify-center opacity-50 hover:opacity-100
+                                transition-opacity duration-300">
+                                    <!-- Linkedin -->
+                                    <a href="  #!" class="flex rounded-full hover:bg-indigo-50 h-10 w-10">
+                                        <i class="mdi mdi-linkedin text-indigo-700 mx-auto mt-2"></i>
+                                    </a>
+
+                                    <!-- Twitter -->
+                                    <a href="  #!" class="flex rounded-full hover:bg-blue-50 h-10 w-10">
+                                        <i class="mdi mdi-twitter text-blue-400 mx-auto mt-2"></i>
+                                    </a>
+
+                                    <!-- Instagram -->
+                                    <a href="  #!" class="flex rounded-full hover:bg-orange-50 h-10 w-10">
+                                        <i class="mdi mdi-instagram text-orange-400 mx-auto mt-2"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="team-card_load_extreme_descripion">
-        <p class=" text-gray-700 text-left text-sm translate-x-2">Lead developer<br> Problem Setter<br>Judge</p>
-    </div>
 </div>
-<div class="team-card-sahith py-12 border-2 border-blue-400" style="transform: translate(-350px,-275px);" >
-    <div class="team-card_load"></div>
-    <div class="team-card_load_extreme_title">
-        <p class=" text-gray-700 text-left text-sm translate-x-2">Sahithi Gandham</p>
-    </div>
-    <div class="team-card_load_extreme_descripion">
-        <p class=" text-gray-700 text-left text-sm translate-x-2">Head of Logistics<br> Asistant Developer<br>Judge</p>
-<<<<<<< HEAD
-    </div>
-
-=======
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
->>>>>>> 28948929e56145eff554a5f10dbee7be4dcbed33
-</section>
-</div>
-
-
