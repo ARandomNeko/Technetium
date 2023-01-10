79f8fd7584dc52c6a02465eaef36aaae59354eaf
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-01-10 08:17:33 +0530

    spacing??

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,20 +1,38 @@
-<figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512">
-  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-    <blockquote>
-      <p class="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
-      </p>
-    </blockquote>
-    <figcaption class="font-medium">
-      <div class="text-sky-500 dark:text-sky-400">
-        Sarah Dayan
-      </div>
-      <div class="text-slate-700 dark:text-slate-500">
-        Staff Engineer, Algolia
-      </div>
-    </figcaption>
-  </div>
-</figure>
+
+<div class="-mb-2 bg-black"  >
+
+<section class="mb-20 translate-y-10">
+     
+  
+<div style = "background-image: url('https://wallpapersmug.com/download/1600x900/dbc266/texture-dark-surface.jpg');background-repeat: no-repeat;  background-size: cover;"class="px-6 py-12 md:px-12 bg-black text-gray-800 text-left lg:text-left block rounded-lg shadow-lg">
+<div class="container mx-auto xl:px-32">
+<div class="grid lg:grid-cols-2 gap-12 items-center">
+<div class="mt-12 lg:mt-0 py-4">
+<div class="bg-black mb-0 py-40">
+
+<div class="team-card-ritu py-12 border-2 border-pink-400   " style="transform: translate(-750px,-275px);" >
+    <div class="team-card_load"></div>
+    <div class="team-card_load_extreme_title">
+        <p class=" text-gray-700 text-left text-sm translate-x-2">Rituparan Reddy</p>
+    </div>
+    <div class="team-card_load_extreme_descripion">
+        <p class=" text-gray-700 text-left text-sm translate-x-2">Lead developer<br> Problem Setter<br>Judge</p>
+    </div>
+</div>
+<div class="team-card-sahith py-12 border-2 border-blue-400" style="transform: translate(-350px,-275px);" >
+    <div class="team-card_load"></div>
+    <div class="team-card_load_extreme_title">
+        <p class=" text-gray-700 text-left text-sm translate-x-2">Sahith Gandham</p>
+    </div>
+    <div class="team-card_load_extreme_descripion">
+        <p class=" text-gray-700 text-left text-sm translate-x-2">Head of Logistics<br> Asistant Developer<br>Judge</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+</div>
+
+
