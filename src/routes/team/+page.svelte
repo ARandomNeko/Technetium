f8f0bddffc82a88e5085f73a260fb7e7731a879d
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-01-11 11:48:07 +0530

    margins for the section header

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -11,7 +11,7 @@
             <!-- Meet the Team -->
             <div class="container max-w-7xl px-4">
                 <!-- Section Header -->
-                <div class="glass flex flex-wrap justify-center text-center mb-2">
+                <div class="glass flex flex-wrap justify-center text-center py-2">
                     <div class="w-full lg:w-6/12 px-4">
                         <!-- Header -->
                         <h1 class="text-gray-100 text-4xl font-bold mb-8">
