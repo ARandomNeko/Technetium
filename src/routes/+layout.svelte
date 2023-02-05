63206809eb6cdd44c54b9892f2ad7837bcbecd03
ARandomNeko <rituparangreddy2006@gmail.com>
2023-02-05 20:01:30 +0530

    literally just the footer cuz fuck vivan

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
   import "@picocss/pico/css/pico.css";
   import "../app.css";
 </script>
-
+<link rel="stylesheet" href="https://cdn.materialdesignicons.com/6.5.95/css/materialdesignicons.min.css">
 
 <header class="py-12">
 
@@ -14,8 +14,8 @@
         <span class="self-center text-xl font-semibold whitespace-nowrap text-white">Technetium</span>
     </a>
     
-    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-      <ul class="flex flex-col p-4 mt-4 border  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 bg-gray-900 ">
+    <div class="items-center justify-between  w-full md:flex md:w-auto md:order-1" id="navbar-default">
+      <ul class="flex  p-4   rounded-lg flex-row space-x-8 mt-0 text-sm font-medium border-0 bg-gray-900 ">
         <li>
           <a href="/" class="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</a>
         </li>
@@ -23,7 +23,7 @@
           <a href="/team" class="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Team</a>
         </li>
         <li>
-          <a href="/itinerary" class="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Itinerary</a>
+          <a href="/about" class="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
         </li>
         <li>
           <a href="#!" class="block py-2 pl-3 pr-4 text-gray-400 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
@@ -37,25 +37,58 @@
 
 <slot/>
 
-<div class ="bg-gray-900 ">
-     <footer class =" py-12 text-right px-24 border-gray-600"> 
-     <a class=" text-gray-400" href="/">Technetium 2023.</a>
-     <p>
-     Made with SvelteKit 
-     </p>
-     </footer>
-</div>
+<footer class ="grid md:grid-cols-2 py-12 justify-items-center px-24 border-gray-600 bg-gray-900 "> 
+  
+  <div>
+    <h1 class="text-4xl md:text-5xl xl:text-6xl  tracking-tight font-semibold text-blue-500 mb-1">
+      Contact Us
+    </h1>
+
+    <h1 class="text-3xl md:text-4xl xl:text-5xl  tracking-tight font-semibold text-gray-300 mb-1">
+      Technetium@sis.edu.in
+    </h1>
+
+    <div class="flex items-center justify-left  hover:opacity-100
+    transition-opacity duration-300 py-8">
+
+        <!-- Linkedin -->
+        <a href=" https://github.com/ARandomNeko" class="flex rounded-2xl hover:bg-indigo-50 h-10 w-10  scale-125">
+            <i class="mdi mdi-github text-indigo-500 mx-auto mt-2"></i>
+        </a>
 
 
-<style>
-    
-    header {
-        display: grid;
 
-        place-items: left;
-      }
+        <!-- Instagram -->
+        <a href=" https://www.instagram.com/arandomneko21/" class="flex rounded-2xl hover:bg-orange-100 h-10 w-10 translate-x-4 scale-125">
+            <i class="mdi mdi-instagram text-orange-400 mx-auto mt-2"></i>
+        </a>
+    </div>
+    <div>
+      <a class=" text-gray-400" href="/">Technetium 2023.</a>
+      <p>
+      By Rituparan Reddy 
+      </p>
+    </div>  
+  </div>
+  <div>
+    <ul class="text-gray-300 text-2xl">
+      <li class="mb-4">
+          <a href="/" class=" hover:underline">Home</a>
+      </li>
+      <li class="mb-4">
+          <a href="/team" class="hover:underline">Team</a>
+      </li>
+      <li class="mb-4">
+          <a href="/about" class="hover:underline">About</a>
+      </li>
+      <li class="mb-4">
+          <a href="#!" class="hover:underline">Register</a>
+      </li>
+  </ul>
+
+  </div>
+  
+</footer>
 
 
-    
- 
- </style>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,23 +2,24 @@
 <!-- Sreenidhi Technetium-->
 <div class=" bg-black"  >
 <!-- Sreenidhi Technetium-->
-    <section class="translate-y-10">
+    <section class=" mt-20 sm:mt-10">
      
   
       <div style = "background-image: url('https://wallpapersmug.com/download/1600x900/dbc266/texture-dark-surface.jpg');background-repeat: no-repeat;  background-size: cover;"class="px-6 py-12 md:px-12 bg-black text-gray-800 text-left lg:text-left block rounded-lg shadow-lg">
         <div class="container mx-auto xl:px-32">
           <div class="items-center">
             <div class="mt-12 lg:mt-0 py-4">
-              <h1 class="typing-title text-xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-1 text-white">Sreenidhi Technetium </h1>
-              <h1 class="text-xl md:text-5xl xl:text-6xl  tracking-tight font-semibold text-blue-500 mb-1">The first student led hackathon in Hyderabad</h1>
+              <h1 class="typing-title text-4xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-1 text-white">Sreenidhi Technetium </h1>
+              <h1 class="text-4xl md:text-5xl xl:text-6xl  tracking-tight font-semibold text-blue-500 mb-1">The first student led hackathon in Hyderabad</h1>
               
             </div>
             <div class="grid lg:grid-cols-2">
               <p class="text-gray-300">
-                Technetium is a 3-day interschool hackathon which will bring together students from across the city of Hyderabad to showcase their skills, collaborate with like-minded individuals, and develop meaningful and innovative solutions to real-world problems. Our theme for the first edition of Technetium is Sustainability, and we encourage you to think outside the box and push the boundaries of what is possible. Whether you are a beginner or a seasoned professional, we have a place for you here. So grab your laptops, charge your devices, and prepare for a truly unforgettable experience!
-
+                Technetium is a 3-day interschool hackathon which will bring together students from across the city of Hyderabad to showcase their skills, collaborate with like-minded individuals, and develop meaningful and innovative solutions to real-world problems. 
+                Our theme for the first edition of Technetium is Sustainability, and we encourage you to think outside the box and push the boundaries of what is possible. Whether you are a beginner or a seasoned professional, we have a place for you here. So grab your laptops, charge your devices, and prepare for a truly unforgettable experience!
+                
               </p>
-            <div class="glass-register rounded-lg shadow-lg bg-gray-800 lg:-translate-y-12">
+            <div class="glass-register rounded-lg shadow-lg bg-gray-800 ">
               <div class="py-4 px-4">
                 <form >
                   <div class="grid md:grid-cols-2 md:gap-2 ">
@@ -50,30 +51,27 @@
 
   
   <!-- Themes and Events -->
-  <section>
-    <div class="transform-gpu flex items-center xs:min-height-fit justify-center bg-black  py-12 pb-48 ">
-      <div class="grid ">
-        <div class="container max-w-7xl px-4">
-          <!-- Section Header -->
-          <div class=" flex flex-wrap justify-right text-left py-2">
-              <div class="glass-content w-full lg:w-5/12 px-4 hover:-translate-x-2 bg-gray-800  hover:rounded-xl hover:drop-shadow-[0_20px_20px_rgba(73,73,73,1)] transition ease-in-out duration-500 ">
-                  <!-- Header -->
-                  <h1 class="text-gray-100 text-4xl font-bold py-4 ">
-                      Themes & Events
-                  </h1>
-  
-                  <!-- Description -->
-                  <p class="text-gray-300 text-2xl font-light">
-                    Over the course of the weekend participate in a multitude of events related to STEM & sustainability
-                  </p>
-              </div>
-              <div class="glass w-full">
-  
-  
-              </div>
+  <section class="translate-y-10">
+    <div style = "background-image: url('https://images.unsplash.com/photo-1526289034009-0240ddb68ce3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80');background-repeat: no-repeat;  background-size: cover;"class="px-6 py-12 md:px-12 bg-black text-gray-800 text-left lg:text-left block rounded-lg shadow-lg">
+      <div class="container mx-auto xl:px-32">
+        <div class="items-center">
+          <div class="mt-12 lg:mt-0 py-4">
+            <h1 class="text-4xl md:text-5xl xl:text-6xl  tracking-tight font-semibold text-blue-500 mb-1">Themes and Events
+
+            </h1>
+            <p class="text-gray-300">
+
+            </p>
           </div>
+        </div>
       </div>
     </div>
   </section>
+
+  <!-- Why participate -->
+
   
-  </div>
+
+
+
+</div>
