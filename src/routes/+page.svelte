e666738c2bcb88ba9046a2c249ee6b67c05b7253
ARandomNeko <53136063+ARandomNeko@users.noreply.github.com>
2023-02-07 10:28:29 +0530

    wait fuck

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -37,7 +37,7 @@
                     
                   </div>
                   <button 
-                  type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="translate-y-3 inline-block px-6 py-4  w-full bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                  type="submit" data-mdb-ripple="true" data-mdb-ripple-color="light" class="translate-y-3 inline-block px-6 py-4  w-full bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                   Notify me
                   </button>
                  
