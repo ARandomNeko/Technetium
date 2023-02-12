24f3418196ddbef1ccb94488b22c8c355e19287e
ARandomNeko <rituparangreddy2006@gmail.com>
2023-02-12 12:52:29 +0530

    removed typing animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,6 +27,15 @@
           });       
         }
 
+
+        var flipdown = new FlipDown(1679632200000);
+        flipdown.start();
+        flipdown.ifEnded(() => {
+          console.log('The countdown has ended!');
+        });
+        var flipdown = new FlipDown(datetime, el, {
+            theme: 'dark' // or light
+        });
       </script>
   
       <div id="vantajs"  class="px-6 py-12 md:px-12 text-left lg:text-left block rounded-lg shadow-lg ">
@@ -34,7 +43,7 @@
         <div class="container mx-auto xl:px-32">
           <div class="items-center">
             <div class="mt-12 lg:mt-0 py-4">
-              <h1 class="typing-title text-4xl md:text-6xl xl:text-6xl font-bold tracking-tight mb-1 text-white">Sreenidhi Technetium </h1>
+              <h1 class=" text-4xl md:text-6xl xl:text-6xl font-bold tracking-tight mb-1 text-white">Sreenidhi Technetium </h1>
               <h1 class="text-4xl md:text-5xl xl:text-6xl  tracking-tight font-semibold text-blue-500 mb-1">The first student led hackathon in Hyderabad</h1>
               
             </div>
@@ -46,26 +55,15 @@
               </p>
             <div class="glass-register rounded-lg shadow-lg ">
               <div class="py-4 px-4">
-                <form >
-                  <div class="grid md:grid-cols-2 md:gap-2 ">
-                    <div class="mb-1">
-                      <input type="first-name" name="First-name"class="form-control block w-full px-3  text-base font-normal text-gray-700 bg-slate-100 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-slate-200 focus:border-blue-600 focus:outline-none" placeholder="First name"/>
-                    </div>
-                    <div class="mb-1">
-                      <input type="last-name" name="Last-name" class="form-control block w-full px-3  text-base font-normal text-gray-700 bg-slate-100 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-slate-200 focus:border-blue-600 focus:outline-none" placeholder="Last name"/>
-                    </div>
-                  </div>
-                  <input type="email"name="Email" class="translate-y-1.5 form-control block w-full px-3 py-1.5 mb-6 text-base font-normal text-gray-700 bg-slate-100 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-slate-200 focus:border-blue-600 focus:outline-none" placeholder="Email address"/>
-                  <div class="form-check flex justify-center mb-1">
-                    
-                  </div>
+                
+                <div id="flipdown" class="flipdown"></div>
                   <a 
                    href="https://forms.gle/7yMcft9NfsRkDuo56"
                    class="translate-y-3 inline-block px-6 py-4  w-full bg-blue-600 text-white text-center font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                   Register
                 </a>
                  
-                </form>
+
               </div>
             </div>
           </div>
