563e1bd4b24ad7cfd6e08bc9a5836abbb1f35ecd
ARandomNeko <rituparangreddy2006@gmail.com>
2023-02-12 13:45:11 +0530

    typing title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,24 +26,14 @@
           maxDistance: 25.00
           });       
         }
-
-
-        var flipdown = new FlipDown(1679632200000);
-        flipdown.start();
-        flipdown.ifEnded(() => {
-          console.log('The countdown has ended!');
-        });
-        var flipdown = new FlipDown(datetime, el, {
-            theme: 'dark' // or light
-        });
       </script>
   
-      <div id="vantajs"  class="px-6 py-12 md:px-12 text-left lg:text-left block rounded-lg shadow-lg ">
+      <div id="vantajs"  class=" typing-title px-6 py-12 md:px-12 text-left lg:text-left block rounded-lg shadow-lg ">
       
         <div class="container mx-auto xl:px-32">
           <div class="items-center">
             <div class="mt-12 lg:mt-0 py-4">
-              <h1 class=" text-4xl md:text-6xl xl:text-6xl font-bold tracking-tight mb-1 text-white">Sreenidhi Technetium </h1>
+              <h1 class="typing-title text-4xl md:text-6xl xl:text-6xl font-bold tracking-tight mb-1 text-white">Sreenidhi Technetium </h1>
               <h1 class="text-4xl md:text-5xl xl:text-6xl  tracking-tight font-semibold text-blue-500 mb-1">The first student led hackathon in Hyderabad</h1>
               
             </div>
@@ -55,9 +45,7 @@
               </p>
             <div class="glass-register rounded-lg shadow-lg ">
               <div class="py-4 px-4">
-                
-                <div id="flipdown" class="flipdown"></div>
-                  <a 
+                <a 
                    href="https://forms.gle/7yMcft9NfsRkDuo56"
                    class="translate-y-3 inline-block px-6 py-4  w-full bg-blue-600 text-white text-center font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                   Register
